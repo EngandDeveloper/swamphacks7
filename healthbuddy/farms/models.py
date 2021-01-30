d33196135b044b1926a446ae5d3a88ccb91575e0
enganddeveloper <46913754+EngandDeveloper@users.noreply.github.com>
2021-01-30 15:31:31 -0500

    latest

--- healthbuddy/farms/models.py
+++ healthbuddy/farms/models.py
@@ -9,5 +9,11 @@ class RegisterUser(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-class LoginUser():
-    print("login")
+class Trainer():
+    print()
+
+class Babysitter():
+    print()
+
+class GymMember():
+    print()
