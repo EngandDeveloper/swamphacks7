b4ff41aa1ec3bad690e61e9d5e34abe600c50c74
enganddeveloper <46913754+EngandDeveloper@users.noreply.github.com>
2021-01-30 23:26:47 -0500

    last recovery

--- healthbuddy/farms/admin.py
+++ healthbuddy/farms/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from farms.models import Trainer, Babysitter, GymMember
 
 # Register your models here.
 admin.site.register(Trainer)
 admin.site.register(Babysitter)
 admin.site.register(GymMember)
-=======
-
-# Register your models here.
->>>>>>> main
